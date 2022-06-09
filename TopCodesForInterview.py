876f804b26418aafb2de910e8622f0f2f413f199
Divyam <divyam2116@gmail.com>
2022-06-09 18:25:09 +0530

    Prime Number in a range

--- TopCodesForInterview.py
+++ TopCodesForInterview.py
@@ -1,5 +1,6 @@
+"""
 # Python program to find whether a given number is prime or not
-# First of all we will 
+# First of all we will create some test cases
 test1 = {
     'input': {
         'num':5
@@ -42,3 +43,57 @@ def is_prime(num):
     return prime
 for i in range(len(tests)):
     print(is_prime(tests[i]['input']['num']) == tests[i]['output'])
+
+"""
+
+# Python program to find prime numbers between a given range
+# First of all we will create some test cases
+
+test1 = {
+    'input': {
+        'start': 2,
+        'end':15
+    },
+    'output': [2, 3, 5, 7, 11, 13]
+}
+test2 = {
+    'input': {
+        'start': 3,
+        'end': 3
+    },
+    'output': [3]
+}
+test3 = {
+    'input': {
+        'start': 15,
+        'end':2
+    },
+    'output': [2, 3, 5, 7, 11, 13]
+}
+
+test4 = {
+    'input': {
+        'start': -2,
+        'end':-1
+    },
+    'output': [-1]
+}
+tests = [test1,test2, test3, test4]
+def prime_in_range(start, end):
+    if start > end:
+        start, end = end, start
+    if end < 2:
+        return [-1]
+    if end == 2:
+        return [2]
+    prime = []
+    for i in range(start, end+1):
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            prime.append(i)
+    return prime
+
+for i in range(len(tests)):
+    print(prime_in_range(tests[i]['input']["start"], tests[i]["input"]["end"]) == tests[i]['output'])
